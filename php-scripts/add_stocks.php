8911249f26ef43c4009e352895a966f936005771
Justin Uppal <uppal2665@gmail.com>
2018-03-12 17:12:47 -0600

    Updated add stock script to include categories

--- php-scripts/add_stocks.php
+++ php-scripts/add_stocks.php
@@ -1,29 +1,55 @@
 <?php
+
+	#include "../routes/includes.php";
+
+	$DBServerName = 'localhost';
+	$DBUserName = 'pi';
+	$DBPassword = 'raspberry';
+	$DBName = 'test';
+	
+	$curl = curl_init();
+	$api_endpoint = 'https://api.iextrading.com/1.0/ref-data/symbols';
+	
+	curl_setopt($curl, CURLOPT_URL, $api_endpoint);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	
+	$result = curl_exec($curl);
+
+	curl_close($curl);
+
+	$result = json_decode($result);
+	
+	$conn = mysqli_connect($DBServerName, $DBUserName, $DBPassword, $DBName);
+	
+	foreach ($result as $stock) 
+	{
+		if ($stock->{"isEnabled"} == true and $stock->{"name"}!="") 
+		{
+			$curl = curl_init();
+			$api_endpoint = 'https://api.iextrading.com/1.0/stock/'.$stock->{"symbol"}.'/company';     
   
-  include "../routes/includes.php";
+			curl_setopt($curl, CURLOPT_URL, $api_endpoint);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-  $curl = curl_init();
-  $api_endpoint = 'https://api.iextrading.com/1.0/ref-data/symbols';     
-  
-  curl_setopt($curl, CURLOPT_URL, $api_endpoint);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			$result2 = curl_exec($curl);
 
-  $result = curl_exec($curl);
-
-  curl_close($curl);
-
-  $result = json_decode($result);
-
-  $conn = mysqli_connect($DBServerName, $DBUserName, $DBPassword, $DBName);
-  foreach ($result as $stock) {
-    if ($stock->{"isEnabled"} == true) {
-      $sql = "INSERT INTO Stocks (symbol, name) VALUES ('" . $stock->{"symbol"} . "', '" . $stock->{"name"} . "');";
-      if ($conn->query($sql)){} else {
-        echo "Insert Failed\n";
-      }
-    }
-  }
-
-  mysqli_close($conn);
-
+			curl_close($curl);
+			
+			$result2 = json_decode($result2);
+			
+			if ($result2->{"sector"}!="" and $result2->{"industry"}!="")
+			{
+				$sql = "INSERT INTO Stocks (symbol, name, sector, industry) VALUES ('" . $stock->{"symbol"} . "', '" . $stock->{"name"} . "', '" . $result2->{"sector"} . "', '" . $result2->{"industry"} . "');";
+				if ($conn->query($sql))
+				{
+					
+				} 
+				else 
+				{
+					echo "Insert Failed\n";
+				}
+			}
+		}
+	}
+	mysqli_close($conn);
 ?>
